63c233c332fe79f3416aa97e1159e5b8720f2e2e
mariainigo97@gmail.com <mariainigo97@gmail.com>
2022-10-20 10:20:20 +0100

    brute force two sum

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,5 @@
+# Easy - Two Sum(https://leetcode.com/problems/two-sum/)
+
 ## DESCRIPTION
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -25,14 +27,27 @@
 
 import unittest
 
+# Brute Force
+def two_sum_BF(nums, target):
+  for i_A in range(len(nums)-1):
+    for i_B in range(i_A+1, len(nums)):
+      if nums[i_A] + nums[i_B] == target:
+        return [i_A, i_B]
+  return None
+
 def two_sum(nums):
   pass
 
 class TestSum(unittest.TestCase):
+  def test_two_sum_BF(self):
+    self.assertEqual(two_sum_BF([2,7,11,15], 9), [0,1])
+    self.assertEqual(two_sum_BF([3,2,4], 6), [1,2])
+    self.assertEqual(two_sum_BF([3,3], 6), [0,1])
+
   def test_two_sum(self):
-    self.assertEqual(two_sum([2,7,11,15]), 9)
-    self.assertEqual(two_sum([3,2,4]), 6)
-    self.assertEqual(two_sum([3,3]), 6)
+    self.assertEqual(two_sum([2,7,11,15], 9), [0,1])
+    self.assertEqual(two_sum([3,2,4], 6), [1,2])
+    self.assertEqual(two_sum([3,3], 6), [0,1])
 
 if __name__ == '__main__':
     unittest.main()
